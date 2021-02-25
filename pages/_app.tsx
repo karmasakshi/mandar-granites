43d8b5f836b35947ac618a4d7ea2634b6245b6e6
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-02-25 21:14:53 +0400

    build: add tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '@styles/global.scss';
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+
+  return <Component {...pageProps} />;
+
+}
+
+export default MyApp;
